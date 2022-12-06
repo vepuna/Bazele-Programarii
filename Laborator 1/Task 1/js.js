c68988e0a9129a181d2f800c8efb9730af0c4764
Cozlov Alexei <alioshavepuna@gmail.com>
2022-12-06 19:14:44 +0300

    lab1 task1

---
+++ Laborator 1/Task 1/js.js
@@ -0,0 +1,7 @@
+const value = parseInt(prompt('Data'));
+
+const monthValue = value * 12;
+const dayValue = value * 365;
+const hoursValue = value * 8766;
+
+alert(`${monthValue} month, ${dayValue} month, ${hours2Value}`);
